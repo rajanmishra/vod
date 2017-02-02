d3574be2bead73045a5d922fed06de3e483c22f4
rajan <rajan>
2017-02-02 13:32:03 +0530

    new mongodb connection

--- public/app/src/js/config.js
+++ public/app/src/js/config.js
@@ -2,7 +2,7 @@
 CONFIG = {
 
 	MOVIES_API: 'https://demo2697834.mockable.io',
-	SERVICE_API: 'http://localhost:3000', 
+	SERVICE_API: 'https://vod-10.herokuapp.com', 
 	DEFAULT_VIDEO_TILE:'http://lorempixel.com/214/317/?t=26'
 };
 /*global variable to set api url etc..*/
