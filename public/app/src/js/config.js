1ad3f51f3e63773798fe0b5f4866d6fe573fcf4e
rajan <rajan>
2017-02-03 00:01:36 +0530

    remove application folder

--- public/app/src/js/config.js
+++
@@ -1,8 +0,0 @@
-/*global variable to set api url etc..*/
-CONFIG = {
-
-	MOVIES_API: 'https://demo2697834.mockable.io',
-	SERVICE_API: 'https://vod-10.herokuapp.com', 
-	DEFAULT_VIDEO_TILE:'http://lorempixel.com/214/317/?t=26'
-};
-/*global variable to set api url etc..*/
