931058baa3104935de03baf2c4e94eee7531206b
rajan <rajan>
2017-02-06 11:40:28 +0530

    paths in config

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -4,13 +4,13 @@ app = angular.module('vod', ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "app/src/html/video_library.html"
+        templateUrl : "src/html/video_library.html"
     })
     .when("/views", {
-        templateUrl : "app/src/html/view_views.html"
+        templateUrl : "src/html/view_views.html"
     })
     .otherwise("/", {
-        templateUrl : "app/src/html/video_library.html"
+        templateUrl : "src/html/video_library.html"
     });
 });
 
